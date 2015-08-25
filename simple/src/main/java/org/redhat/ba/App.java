7a208b9e2e1c37f003d0d2568bf4d8da58ff9b66
Talal Qadri <tqadri@tqadri.aus.csb>
2015-08-25 10:04:10 -0400

    Created initial maven project

---
+++ simple/src/main/java/org/redhat/ba/App.java
@@ -0,0 +1,13 @@
+package org.redhat.ba;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
